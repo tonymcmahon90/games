6ecc868682713d7b4c02efde7f28d59f58bd0b4f
tony <137812923+tonymcmahon90@users.noreply.github.com>
2025-04-26 17:41:38 +0100

    Add files via upload

--- direct2d ping pong.cpp
+++ direct2d ping pong.cpp
@@ -4,10 +4,10 @@
 #include <d2d1_1.h>
 #include <strsafe.h>
 #include <windowsx.h> // GET_X_LPARAM
-#include <math.h>
-#include "resource.h"
-
-#define FPS 10
+#include <math.h> // fabs
+#include "resource.h" // icon
+#define FPS 60
+#define BALLSPEED 0.002
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 bool InitD2D(HWND hwnd);
@@ -21,56 +21,42 @@ ID2D1SolidColorBrush* pBrush = NULL;
 ID2D1LinearGradientBrush* pLinBrush = NULL; // https://learn.microsoft.com/en-us/windows/win32/direct2d/direct2d-brushes-overview 
 ID2D1RadialGradientBrush* pGradBrush = NULL;
 
-LONG width, height,initwidth,initheight;
-float user_bat_x = 100, user_bat_y = 300,mouse_x=-1,mouse_y=-1,ball_x=600,ball_y=250;
-float ball_play = -1,ball_x_direction=-1,ball_y_direction=-1;
-float ai_y = 300;
+LONG width,height;
+bool game_play = false;
+float user_bat_x = 0.1, user_bat_y = 0.5,mouse_x=-1,mouse_y=-1,ball_x=0.5,ball_y=0.5,ball_x_direction,ball_y_direction,ai_y = 0.5,bat_size=0.035,ball_speed=BALLSPEED,ball_accel=1+(BALLSPEED/5);
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR cmd, int show)
 {
-	WNDCLASS wc = { CS_HREDRAW | CS_VREDRAW,WindowProc,0,0,hInst,(HICON)LoadImage(hInst,MAKEINTRESOURCE(IDI_ICON1),IMAGE_ICON,LR_DEFAULTSIZE,LR_DEFAULTSIZE,LR_SHARED)
-		//LoadIcon(NULL, IDI_APPLICATION)
+	WNDCLASS wc = { CS_HREDRAW | CS_VREDRAW,WindowProc,0,0,hInst,(HICON)LoadImage(hInst,MAKEINTRESOURCE(IDI_ICON2),IMAGE_ICON,LR_DEFAULTSIZE,LR_DEFAULTSIZE,LR_SHARED)		
 		,LoadCursor(NULL, IDC_ARROW),NULL,NULL,L"Window" };
 	RegisterClass(&wc);
 
 	HWND hwnd = CreateWindow(L"Window", L"Direct2D Ping Pong", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInst, NULL);
 	ShowWindow(hwnd, show);
-
-	RECT rc;
-	GetClientRect(hwnd, &rc);
-	initwidth = rc.right - rc.left;
-	initheight = rc.bottom - rc.top;
 	InitD2D(hwnd);
 			
 	LARGE_INTEGER qtime,qfreq,qtemp;
 	QueryPerformanceCounter(&qtime);
 	QueryPerformanceFrequency(&qfreq);
 	qfreq.QuadPart = qfreq.QuadPart / FPS;
-
-	// WCHAR txt[50]; StringCbPrintfW(txt,sizeof(WCHAR)*50, L" F=%d", qfreq.QuadPart); MessageBox(NULL,txt, NULL, 0);
-	
+		
 	MSG msg = {};
 	while (true)
 	{
 		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
 		{			
 			if (msg.message == WM_QUIT) break;
-
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);			
 		}
 		else
 		{
 			QueryPerformanceCounter(&qtemp);
-
-			if (qtemp.QuadPart < ( qtime.QuadPart + qfreq.QuadPart) ) continue;
-				
+			if (qtemp.QuadPart < ( qtime.QuadPart + qfreq.QuadPart) ) continue;				
 			Paint();
 			qtime.QuadPart = qtemp.QuadPart;
 		}
-	}
-
-	// MessageBox(NULL, L"Done", NULL, NULL);
+	}		
 	EndD2D();
 	return 0;
 }
@@ -78,32 +64,29 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR cmd, int show)
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (uMsg)
-	{
-	//case WM_PAINT: Paint(); break; // now using the timer 
+	{	
 	case WM_CLOSE: DestroyWindow(hwnd); break;
 	case WM_DESTROY: PostQuitMessage(0); break;
 	case WM_SIZE: Resize(hwnd); break;
-	case WM_MOUSEMOVE: 
-		if (MK_RBUTTON && wParam) { user_bat_x = 100; user_bat_y = 300; mouse_x = GET_X_LPARAM(lParam); mouse_y = GET_Y_LPARAM(lParam); return 0; } // reset 
-
-		if (mouse_x == -1 || MK_LBUTTON && wParam) // left click to begin or reset bat movement 
+	case WM_MOUSEMOVE: 		
+		if (MK_LBUTTON && wParam) // left click to begin 
 		{
-			if (MK_LBUTTON && wParam) { mouse_x = GET_X_LPARAM(lParam); mouse_y = GET_Y_LPARAM(lParam); } // start when use clicks left mouse
+			mouse_x = GET_X_LPARAM(lParam); 
+			mouse_y = GET_Y_LPARAM(lParam);  // start when use clicks left mouse
 			return 0;
 		}	
 		
 		int x_diff= GET_X_LPARAM(lParam) - mouse_x;
 		int y_diff= GET_Y_LPARAM(lParam) - mouse_y;
-		user_bat_x += x_diff;
-		user_bat_y += y_diff;
+		user_bat_x += (float)x_diff/width;
+		user_bat_y += (float)y_diff/height;
 		mouse_x = GET_X_LPARAM(lParam);
 		mouse_y = GET_Y_LPARAM(lParam);
 
-		if (user_bat_x < 50)user_bat_x = 50;
-		if (user_bat_x > 250)user_bat_x = 250;
-
-		if (user_bat_y < 100)user_bat_y = 100;
-		if (user_bat_y > 500)user_bat_y = 500;
+		if (user_bat_x < 0.1)user_bat_x = 0.1;
+		if (user_bat_x > 0.45)user_bat_x = 0.45;
+		if (user_bat_y < 0.1)user_bat_y = 0.1;
+		if (user_bat_y > 0.9)user_bat_y = 0.9;
 		return 0;
 	}
 	return DefWindowProc(hwnd, uMsg, wParam, lParam);
@@ -141,88 +124,62 @@ void Paint()
 {
 	if (pRender == NULL || pBrush == NULL) return;
 
-	float xscale = width / initwidth,yscale=height/initheight;
-
 	pRender->BeginDraw();
 	pRender->Clear(D2D1::ColorF(D2D1::ColorF::White));
 
 	// table edges and middle
 	pBrush->SetColor(D2D1::ColorF(D2D1::ColorF::Black));
-	pRender->DrawRectangle(D2D1::RectF(100,100,900,100), pBrush,10.f); // top bar
-	pRender->DrawRectangle(D2D1::RectF(100,500,900,500), pBrush, 10.f); // bottom bar
-	pRender->DrawRectangle(D2D1::RectF(500, 100, 500, 500), pBrush, 10.f); // middle
+	pRender->DrawRectangle(D2D1::RectF(0.1*width,0.1*height,0.9*width,0.1*height), pBrush,0.005*width); // top bar
+	pRender->DrawRectangle(D2D1::RectF(0.1*width,0.9*height,0.9*width,0.9*height), pBrush,0.005*width); // bottom bar
+	pRender->DrawRectangle(D2D1::RectF(0.5*width,0.1*height,0.5*width,0.9*height), pBrush,0.005*width); // middle
 
 	// bats
-	pRender->DrawRectangle(D2D1::RectF(user_bat_x, user_bat_y-50,user_bat_x, user_bat_y+50), pBrush, 10.f); // left user
-	pRender->DrawRectangle(D2D1::RectF(900, ai_y-50, 900, ai_y+50), pBrush, 10.f); // right AI
+	pRender->DrawRectangle(D2D1::RectF(user_bat_x*width, (user_bat_y-bat_size)*height,user_bat_x*width, (user_bat_y + bat_size) * height ),  pBrush, 0.005 * width); // left user
+	pRender->DrawRectangle(D2D1::RectF(0.9*width, (ai_y - bat_size) * height, 0.9*width, (ai_y + bat_size) * height ), pBrush, 0.005 * width); // right AI
 
 	// ball
-	pRender->DrawRectangle(D2D1::RectF(ball_x,ball_y,ball_x,ball_y), pBrush, 15.f); // ball
+	pRender->DrawRectangle(D2D1::RectF(ball_x*width,ball_y*height,ball_x*width,ball_y*height), pBrush, 0.0075 * width); // ball
 
-	if (ball_play == -1) { // start game 
-		ball_x_direction = -10;
-		ball_y_direction = -10;
-
-		ball_x += ball_x_direction;
-		ball_y += ball_y_direction;
-		ball_play = 0;
+	if (!game_play) { // start game 
+		ball_speed = BALLSPEED;
+		ball_x_direction = -ball_speed;
+		ball_y_direction = -ball_speed;		
+		game_play = true;
 	}
 
-	if(ball_x-user_bat_x<20 && abs((int)ball_y-(int)user_bat_y)<50) ball_x_direction = 10;
+	// bounce off user bat
+	if (fabs(ball_x-user_bat_x)<0.01 && fabs(ball_y - user_bat_y)<bat_size) ball_x_direction=ball_speed;
 
-	// bounce around table
+	// ball lost
+	if (ball_x < 0.09) { MessageBox(NULL, L"Opponent wins a point", L"", MB_OK); ball_x = 0.5; ai_y = 0.5; game_play = false; }
+	if (ball_x > 0.91) { MessageBox(NULL, L"You win a point", L"", MB_OK); ball_x = 0.5; ai_y = 0.5; game_play = false; }
 
-	if (ball_x < 50) {
-		MessageBox(NULL, L"Opponent wins a point", NULL, MB_OK); ball_x = 500;
-	}
-	if (ball_y < 110) ball_y_direction = 10;
-	if (ball_x > 900) {
-		MessageBox(NULL, L"You win a point", NULL, MB_OK); ball_x = 500; ai_y = 300;
-	}
-	if (ball_y > 490) ball_y_direction = -10;
+	// ball bounce off top or bottom
+	if (ball_y-ball_speed-0.005 < 0.1) ball_y_direction = ball_speed;
+	if (ball_y+ball_speed+0.005 > 0.9) ball_y_direction = -ball_speed;
 
-	// bounce off bat 
+	// bounce off AI bat 
+	if (fabs(0.9-ball_x)<0.01 && fabs(ball_y - ai_y) < bat_size) ball_x_direction = -ball_speed;
 
-	if (ball_x - user_bat_x < 20 && abs((int)ball_y - (int)user_bat_y) < 50) ball_x_direction = 10;
+	// AI respond once ball is on side
+	if (ball_x > 0.5){ if (ai_y < ball_y) ai_y += ball_speed; else if (ai_y > ball_y) ai_y -= ball_speed; }	
 
-	if (900-ball_x < 20 && abs((int)ball_y - (int)ai_y) < 50) ball_x_direction = -10;
-
+	// move ball
 	ball_x += ball_x_direction;
 	ball_y += ball_y_direction;
-
-	// AI
-
-	if (ball_x > 700)
-	{
-		if (ai_y < ball_y) ai_y += 10; else if (ai_y > ball_y) ai_y -= 10;
-	}
-	/*
-	D2D1_ELLIPSE ellipse;
-
-	for (int i = 0; i < 10; i++)
-	{
-		pBrush->SetColor(D2D1::ColorF((rand() % 200) / 200.f, (rand() % 200) / 200.f, (rand() % 200) / 200.f));
-		ellipse = D2D1::Ellipse(D2D1::Point2F((float)(rand() % 1000), (float)(rand() % 1000)), (float)(rand() % 1000), (float)(rand() % 1000));
-		pRender->DrawEllipse(ellipse, pBrush, (float)(rand() % 20));
-
-		pRender->DrawRectangle(D2D1::RectF((float)(rand() % 1000), (float)(rand() % 1000), (float)(rand() % 1000), (float)(rand() % 1000)), pBrush, (float)(rand() % 20));
-	}*/
+	ball_speed *= ball_accel;
 	pRender->EndDraw();
 }
 
 bool Resize(HWND hwnd)
 {
 	if (pRender == NULL) return false;
-
 	RECT rc;
 	GetClientRect(hwnd, &rc);
 	width = rc.right - rc.left;
 	height = rc.bottom - rc.top;
-
 	D2D1_SIZE_U pixels = D2D1::SizeU(width, height);
 	HRESULT hr = pRender->Resize(&pixels);
-
 	if (FAILED(hr)) return false;
-
 	return true;
 }
